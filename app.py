fb6708918b36e0abf6f567dd9a01412ed8680a80
Amir <amirligvany@gmail.com>
2024-11-08 14:21:14 +0330

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import time
+
+ui = Tk()
+ui.title("saat")
+ui.geometry('600x300')
+
+label = Label(ui, text="saat bahadi", font=(None, 72), fg='blue', bg='yellow')
+label.pack()
+
+
+ui.mainloop()
