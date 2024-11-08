34c4a653c1dda53f576a511f1d6a54a21525b417
Amir <amirligvany@gmail.com>
2024-11-08 15:07:23 +0330

    end

--- app.py
+++ app.py
@@ -12,20 +12,17 @@ def CurrentTime():
     S = time.strftime("%S")
     am_pm = time.strftime("%p")
     week_day = time.strftime("%A")
-    zone = time.strftime("%Z")
-
-    txt = week_day + "\n" + h + ":" + m + ":" + S + "" + "\n" + am_pm
-    txt2 = zone
+    day = time.strftime("%d")
+    txt = week_day + " " + day + "th" + "\n" + \
+        h + ":" + m + ":" + S + "" + "\n" + am_pm
     label.config(text=txt)
     label.after(1000, CurrentTime)
-    label2.config(text=txt2)
-    label2.after(1000, CurrentTime)
 
 
 label = Label(ui, text="", font=(None, 62), fg='blue', bg='skyblue')
-label2 = Label(ui, text="", font=(None, 24), bg="skyblue", fg="blue")
+
 label.pack()
-label2.pack()
+
 CurrentTime()
 
 ui.mainloop()
